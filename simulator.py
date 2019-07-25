30225a051fdefe46692191071c83c7f6ee46452e
newbeeH <dzzxhushengjie@126.com>
2019-07-25 23:09:35 +0800

    Update flow table updating module

--- simulator.py
+++ simulator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import time
+from squeue import Flow, Squeue
 
 class simulator(object):
     def __init__(self):
@@ -9,8 +10,9 @@ class simulator(object):
         self.t_interval = 1000  #training interval 
         self.sendingqueues = []     #store the uncompleted flows
         self.completedqueues = []   #store the completed flows
+        self.bandwidth = 1000000000 #simulation bandwidth, unit:b/s
+        self.hpc = 0                #counter of high priority flow 
 
-    ###############debug#########################
     def prepare(self, temptime):
         '''
         Adding new flow to sending queues according to the input trace data
@@ -19,14 +21,27 @@ class simulator(object):
         for index, row in self.data.iterrows():
             interval = row['rtime']
             if temptime - self.starttime > interval:
-                pass
-    ###############debug#########################
-    def updatequeue(self,interval):
+                f = Flow(row)
+                s = Squeue(f, temptime)
+                self.hpc += 1
+                self.sendingqueues.append(s)
+                self.data.drop(index)
+            else:
+                break
+
+    def updatequeue(self, interval, temp):
         '''
         Update info in each sending queues and change the state of each flow
-        '''
-        for queue in self.s_queues:
-            pass
+        '''        
+        for i in range(len(sendingqueues)-1, -1, -1):
+            queue = sendingqueues[i]
+            if queue.priority:
+                bw = int(self.bandwidth / self.hpc)
+                ret = queue.update(bw, interval, temp)
+                if ret:
+                    self.completedqueues.append(queue)
+                    self.sendingqueues.pop(i)
+
     ###############debug#########################
     def train():
         pass
@@ -46,7 +61,7 @@ class simulator(object):
             temp = nowTime(time.time())
             interval = temp - p_last
             p_last = temp
-            updatequeue(interval)
+            updatequeue(interval, temp)
             prepare(temp)
             if(temp - t_last > t_interval):
                 train()
