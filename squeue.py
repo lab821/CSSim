74ff9cc58716b16c3507ecca83514cf412eaa140
newbeeH <dzzxhushengjie@126.com>
2019-07-25 22:40:53 +0800

    Complete the design of sending queue

--- squeue.py
+++ squeue.py
@@ -10,6 +10,7 @@ class Flow(object):
         self.dp = row['dp']         #dst port
         self.size = row['size']     #flow size    
         self.tag = row['tag']       #flow tag (for identification)
+        self.protocol = 6           #protocol
 
     def flowinfo(self):
         '''
@@ -19,13 +20,16 @@ class Flow(object):
 
 
 class Squeue(object):
-    def __init__(self, flow):
+    def __init__(self, flow, starttime):
         self.flow = flow    #infomation of this flow
         self.residualsize = flow['size']    #residual size of this flow
         self.priority = 1   #the sending priority
         self.status = 0     #sending status, 0 for uncompleted and 1 for completed
+        self.starttime = starttime  #the start time 
+        self.duration = -1          #the duration of transmission
 
-    def update(self, bandwidth, interval):
+
+    def update(self, bandwidth, interval, temp):
         '''
         update the residual size and status of this flow
         if the residual size < 0 then this flow is completed and return 1
@@ -35,6 +39,7 @@ class Squeue(object):
         self.residualsize = self.residualsize - sentsize
         if self.residualsize <= 0:
             self.status = 1
+            self.duration = temp - self.starttime
             return 1
         else:
             return 0
@@ -46,4 +51,21 @@ class Squeue(object):
         There are two model of this function:
         if the flow is completed return the inde0
         '''
-        pass
+        res = {}
+        if self.status :
+            res['src'] = self.flow['src']
+            res['dst'] = self.flow['dst']
+            res['protocol'] = self.flow['protocol']
+            res['sp'] = self.flow['sp']
+            res['dp'] = self.flow['dp']
+            res['priority'] = self.priority
+            return res
+        else:
+            res['src'] = self.flow['src']
+            res['dst'] = self.flow['dst']
+            res['protocol'] = self.flow['protocol']
+            res['sp'] = self.flow['sp']
+            res['dp'] = self.flow['dp']
+            res['duration'] = self.duration
+            res['size'] = self.flow['size']
+            return res           
