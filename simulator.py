b67ccf4e25cba23829f5f5439d664598884cb0b7
newbeeH <dzzxhushengjie@126.com>
2019-07-26 11:07:17 +0800

    Adding control function and getinfo function for scheduler

--- simulator.py
+++ simulator.py
@@ -1,8 +1,12 @@
+#The simulator main module
+
 import numpy as np
 import pandas as pd
 import time
 from squeue import Flow, Squeue
+from scheduler import scheduler
 
+#TODO: adding the trace file 
 class simulator(object):
     def __init__(self):
         #self.data = pd.read_csv(trace)
@@ -42,11 +46,34 @@ class simulator(object):
                     self.completedqueues.append(queue)
                     self.sendingqueues.pop(i)
 
-    ###############debug#########################
-    def train():
-        pass
+    def Getinfo(self):
+        '''
+        Get the current active queues and completion queues information
+        Two tables are returned, representing the active queues and the completed queues, respectively
+        ret unit : dataframe  
+        '''
+        actq = pd.DataFrame(columns=['src', 'dst','protocol', 'sp', 'dp', 'priority'])
+        cptq = pd.DataFrame(columns=['src', 'dst','protocol', 'sp', 'dp', 'duration', 'size'])
+        for queue in sendingqueues:
+            row = queue.Getinfo()
+            actq = actq.append(row, ignore_index=True)
+        for queue in completedqueues:
+            row = queue.Getinfo()
+            cptq = cptq.append(row, ignore_index=True)
+        return actq, cptq
+    
+    def control(self, res):
+        '''
+        Rate control by modifying the priority of sending queue
+        Implementation based on the results returned by the scheduler
+        '''
+        for i in res.keys():
+            change = res[i] - sendingqueues[i].priority
+            sendingqueues[i].priority = res[i]
+            hpc = hpc + change  #change the high priority flow counter
 
-    ###############debug#########################
+
+    ###############TODO: make the control into non-blocking control#########################
     def run(self):
         '''
         The simulation main function
@@ -61,15 +88,17 @@ class simulator(object):
             temp = nowTime(time.time())
             interval = temp - p_last
             p_last = temp
-            updatequeue(interval, temp)
-            prepare(temp)
+            self.updatequeue(interval, temp)
+            self.prepare(temp)
             if(temp - t_last > t_interval):
-                train()
+                actq, cptq = self.Getinfo()
+                res = scheduler(actq, cptq)
+                self.control(res)
                 t_last = temp
             else:
                 time.sleep(self.p_interval/1000)
 
-###############debug#########################
+#TODO: adding the trace file 
 if __name__ == "__main__":
     sim = simulator()
     sim.run()
